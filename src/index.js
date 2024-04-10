6c540d1ca5044a36e26ea76c1e2a18a7cb500200
Omolwethu <137441963+Omolwethu@users.noreply.github.com>
2024-04-10 22:09:16 +0200

    Imported Weather.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Weather from "./Weather";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="Forecast">
+      <h1>Weather App</h1>
+      <Weather />
+      <p>
+        <a
+          href="https://github.com/Omolwethu?tab=repositories"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Open Source Code
+        </a>{" "}
+        by Olwethu Zulu (Omolwethu)💜
+      </p>
+    </div>
   </React.StrictMode>
 );
 
